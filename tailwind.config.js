6f872025f01e3d138f000fd82a69b81d224a4891
Oğuzhan Altun <altunnoguzhan@gmail.com>
2023-12-05 00:11:26 +0300

    background changed
    background changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,7 @@ export default {
       },
     },
     backgroundImage: {
-      mainImage:
-        "url('https://s3-alpha-sig.figma.com/img/7816/eafa/1bceefb1d3ea5b7d0f528d6d9e8b16e3?Expires=1700438400&Signature=eObKAvNorTZvflxb6CwUxRKHwFL2apWAij~9p0oTmkcmRsYwTuxinTml0olX~N1vCXGNu7pDhC9MVclDNXp-CSWRqiZ6oWnyjOldG~8NKl59XmGHUKRN3UY4ZoDolouFKrf4fRp3Az~s3CPhOhRjKV0igBbtUrwBQG~8TQYPdqDESRN~Dl53JZFYf0rfG6qTdtjCXNvEDIz6vsWElJ55SL9lzwtmnmYNRsABhFwpjsJdSLpb4mBg2lPjIPiu~UvUmKPEGA~h9X2GWa9-UULHLJ5TMjdld9BmzbRym~rIXvLICbago1koypkzAeLitiJU89u7O1pIh392Y3ZJBuMZMg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4')",
+      mainImage: "url('main-bg.jpg')",
     },
     borderRadius: {
       priv: "27.244px",
